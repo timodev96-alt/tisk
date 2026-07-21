7b96ca0b703768c331fbd77537e12d9e6bc291d6
Timothy <timo.dev96@gmail.com>
2026-07-21 20:31:11 +0300

    added README.md as project description for pypi!

--- setup.py
+++ setup.py
@@ -1,9 +1,15 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
 
 setup(
     name='tisk',
-    version='0.1.0',
+    version='0.1.1',
     description='Simple but cool Task manager',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
     install_requires=['rich'],
     author='Timothy Emad',
     entry_points={
